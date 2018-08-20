fb1a89416794968d947d31b608b517c806f03d69
Michael Lionardi <michael.lionardi@telekom.de>
2018-08-20 12:43:18 +0200

    anpassung html

--- src/main/java/Form.java
+++ src/main/java/Form.java
@@ -21,7 +21,7 @@ public class Form extends HttpServlet {
       response.setContentType("text/html");
 
       PrintWriter out = response.getWriter();
-      String title = "Using GET Method to Read Form Data";
+      String title = "POST Form";
       String docType =
          "<!doctype html public \"-//w3c//dtd html 4.0 " +
          "transitional//en\">\n";
