0acec80ae7be16a726cf29f8c7bd2d70e964b506
Michael Lionardi <michael.lionardi@telekom.de>
2018-09-05 15:07:08 +0200

    huhu added

--- src/main/java/ReadParams.java
+++ src/main/java/ReadParams.java
@@ -28,7 +28,7 @@ public void doGet(HttpServletRequest request, HttpServletResponse response)
       "<h1 align = \"center\">" + title + "</h1>\n" +
       "<table width = \"100%\" border = \"1\" align = \"center\">\n" +
       "<tr bgcolor = \"#949494\">\n" +
-         "<th>huhuParam Name</th>" + 
+         "<th>huhu Param Name</th>" + 
          "<th>Param Value(s)</th>\n"+
       "</tr>\n"
    );
